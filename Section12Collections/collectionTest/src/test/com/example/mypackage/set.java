ce147058b0bd4319471d4d6cf66653ef5427e2be
Johnmai1994 <mjd64929@icloud.com>
2020-02-27 14:29:15 -0500

    HashSet
    根据存储的元素计算出hashCode值，然后根据计算得出的hashCode值和数组的长度进行计算出存储的下标；如果下标的位置无元素，那么直接存储；如果有元素，那么使用要存入的元素和该元素进行equals方法，如果结果为真，则已经有相同的元素了，所以直接不存；如果结果假，那么进行存储，以链表的形式存储。

---
+++ Section12Collections/collectionTest/src/test/com/example/mypackage/set.java
@@ -0,0 +1,99 @@
+package test.com.example.mypackage;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class set {
+
+    @Test
+    public void test2() {
+        class Person{
+            private String name;
+            private int age;
+
+            public Person(String name, int age) {
+                this.name = name;
+                this.age = age;
+            }
+
+            // 要让哈希表存储不重复的元素，就必须重新写hashCode和equals方法
+
+            @Override
+            public int hashCode() {
+                final int prime = 31;
+                int result = 1;
+                result = prime*result + age;
+                result = prime*result + name.hashCode();
+
+                return result;
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                if (this == obj) {
+                    return true;
+                }
+                if (obj == null) {
+                    return false;
+                }
+                if (getClass() != obj.getClass()) {
+                    return false;
+                }
+                Person other = (Person) obj;
+                if (age != other.age) {
+                    return false;
+                }
+                if (!name.equals(other.name)) {
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            public String toString() {
+                return "Person [name: " + name + " , age: " + age + "]";
+            }
+        }
+
+        Set<Person> set = new HashSet<Person>();
+
+        set.add(new Person("John", 12));
+        set.add(new Person("Mike", 12));
+        set.add(new Person("SB Tom", 12));
+        set.add(new Person("John", 12));
+
+        for (Person p : set) {
+            System.out.println(p);
+        }
+
+        //所以在向HashSet集合中存储自定义对象时，为了保证set集合的唯一性，那么必须重写hashCode和equals方法。
+
+
+    }
+
+
+    @Test
+    public void test1(){
+        Set<String>  set = new HashSet<>();
+        set.hashCode();
+        set.add("111");
+        set.add("11");
+        set.add("11");
+        set.add("abc");
+
+        Iterator<String> it = set.iterator();
+        while (it.hasNext()) {
+            String str = it.next();
+            System.out.println(str);
+            // 这里的打印序列，是根据str.hashCode()的序列排序的
+            //   str.hashCode 越小，则排越前
+            System.out.println(str + " hashCode is: "+ str.hashCode());
+        }
+
+
+    }
+
+}
