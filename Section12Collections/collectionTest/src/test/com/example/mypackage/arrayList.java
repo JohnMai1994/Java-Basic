6b8fc8cd998154a500cbbbb3bb7464e67e18f33a
Johnmai1994 <mjd64929@icloud.com>
2020-02-27 14:28:52 -0500

    Update arrayList Note

--- Section12Collections/collectionTest/src/test/com/example/mypackage/arrayList.java
+++ Section12Collections/collectionTest/src/test/com/example/mypackage/arrayList.java
@@ -2,18 +2,48 @@ package test.com.example.mypackage;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 
 public class arrayList {
 
+    @Test
+    public void Queue() {
+        class QueueTest {
+            private LinkedList<String > linkedList = new LinkedList<String>();
+            // Push
+            public void push(String str) {
+                linkedList.addFirst(str);
+            }
+
+            // remove
+            public String pop() {
+                return linkedList.removeLast();
+            }
+
+            // isEmpty
+            public Boolean isEmpty() {
+                return linkedList.isEmpty();
+            }
+        }
+
+        QueueTest queue = new QueueTest();
+        queue.push("我是第一个进的");
+        queue.push("我是第二个进的");
+        queue.push("我是第三个进的");
+        queue.push("我是第四个进的");
+        queue.push("我是第五个进的");
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.pop());
+        }
+
+    }
+
 
     @Test
-    public void test2() {
-        class stack {
+    public void Stack() {
+        class StackTest {
             private LinkedList<String > linkedList = new LinkedList<String>();
 
             // Push
@@ -27,7 +57,7 @@ public class arrayList {
             }
 
 
-            // peek
+            // peek 查看栈顶对象而不移除它
             public String peek() {
                 return linkedList.peek();
             }
@@ -36,9 +66,25 @@ public class arrayList {
             public Boolean isEmpty() {
                 return linkedList.isEmpty();
             }
-
-
         }
+
+        // 存入
+        StackTest stack = new StackTest();
+        stack.push("第一个进入的");
+        stack.push("第二个进入的");
+        stack.push("第三个进入的");
+        stack.push("第四个进入的");
+        stack.push("第五个进入的");
+        // 取出
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+        // 查看 查看最顶的对象
+        stack.push("Hello");
+        System.out.println(stack.peek());
+        stack.push("你好吗");
+        System.out.println(stack.peek());
+
     }
 
 
